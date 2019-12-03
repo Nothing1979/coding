ac55a58d1d48958f586879be6a6812827635c09c
Kenneth Hesselholdt <khe@pixxxel.dk>
2019-12-03 23:06:56 +0100

    NA

--- gulp-4/gulpfile.js
+++ gulp-4/gulpfile.js
@@ -1,32 +1,32 @@
-//let themename = "gulp-4"
+let themename = "gulp-4"
 
 let gulp = require("gulp"),
     autoprefixer = require("gulp-autoprefixer"),
     browserSync = require("browser-sync").create(),
-    //reload = browserSync.reload,
+    reload = browserSync.reload,
     sass = require("gulp-sass"),
     cleanCSS = require("gulp-clean-css"),
     sourcemaps = require("gulp-sourcemaps"),
     concat = require("gulp-concat"),
-    //imagemin = require("gulp-imagemin"),
-    //change = require("gulp-changed"),
-    //uglify = require("gulp-uglify"),
+    imagemin = require("gulp-imagemin"),
+    change = require("gulp-changed"),
+    uglify = require("gulp-uglify"),
     lineec = require("gulp-line-ending-corrector"),
     stylus = require("gulp-stylus")
 
 let root = "./", //+ themename + "/",
     scss = root + "sass/",
-    styl = root + "styl/"/*
+    styl = root + "styl/",
     js = root + "js/",
-    jsdist = root + "dist/js"*/
+    jsdist = root + "dist/js"
 
 let sassWatchFiles = scss + "**/*.scss"
 let styleWatchFiles = styl + "**/*.styl"
 
-/*let jsSRC = [
+let jsSRC = [
     js + "na.js",
     js + "na.js"
-]*/
+]
 
 var cssSRC = [
     /*root + "src/css/na.css",
@@ -35,8 +35,8 @@ var cssSRC = [
     root + "default.css"
 ]
 
-/*let imgSRC = root + "src/images/*",
-    imgDEST = root + "dist/images"*/
+let imgSRC = root + "src/images/*",
+    imgDEST = root + "dist/images"
 
 function SASScss() {
     return gulp.src([scss + "default.scss"])
@@ -80,7 +80,7 @@ function STYLconcatCSS() {
     .pipe(gulp.dest(styl))
 }
 
-/*function javascript() {
+function javascript() {
     return gulp.src(jsSRC)
     .pipe(concat("default.js"))
     .pipe(uglify())
@@ -97,7 +97,7 @@ function imgmin() {
         imagemin.optipng({optimizationLevel: 5})
     ]))
     .pipe(gulp.dest(imgDEST))
-}*/
+}
 
 function watch() {
     browserSync.init({
